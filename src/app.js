21e579e237a40d185664fbe0b55e10571681b769
rezatd-develop <reza.td.develop@gmail.com>
2026-07-22 11:15:16 +0330

    improve cors for production

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  credentials: true
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
