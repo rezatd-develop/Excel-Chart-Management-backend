5c2a3e0aa8977188892235503e46a44f7bb989ab
rezatd-develop <reza.td.develop@gmail.com>
2026-07-22 10:55:46 +0330

    settings for deployment

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import app from "./src/app.js";
 
 dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb://localhost:27017/my_node_app');
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("Database connection failed:", error.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("MongoDB Connected");
+    } catch (error) {
+        console.error("Database connection failed:", error.message);
+        process.exit(1);
+    }
 };
